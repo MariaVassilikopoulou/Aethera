1bd828bdefd0e6a340777201caca7083b8e1aa4c
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-08-05 17:35:35 +0200

    redirect uri correction

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,7 +4,8 @@ export const msalConfig = {
     auth: {
       clientId: "e83f31c8-cc33-49ab-b023-6091f20f85ed",
       authority: "https://aetheraparfum.ciamlogin.com/fdc03cff-281e-4d9e-abf7-f454c53775e4",
-      redirectUri: "http://localhost:3000", 
+      redirectUri: isLocalhost ? "http://localhost:3000" : "https://aethera-eight.vercel.app",
+    
     },
     cache: {
       cacheLocation: "localStorage",
