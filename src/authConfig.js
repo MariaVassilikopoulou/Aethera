c9acc684c6da91d0e19dc72d1ce7473644957ed6
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-08-21 15:56:00 +0200

    scope new

--- src/authConfig.js
+++ src/authConfig.js
@@ -6,8 +6,16 @@ const isLocalhost = typeof window !== 'undefined' && window.location.hostname ==
 export const msalConfig = {
     auth: {
       clientId: "e83f31c8-cc33-49ab-b023-6091f20f85ed",
+      /*authority: "https://aetheraparfum.ciamlogin.com/aetheraparfum.onmicrosoft.com/userflownew"
+                 
+      , // ✅ FIXED*/
       authority: "https://aetheraparfum.ciamlogin.com/fdc03cff-281e-4d9e-abf7-f454c53775e4",
-      redirectUri: isLocalhost ? "http://localhost:3000" : "https://aethera-eight.vercel.app",
+     // redirectUri: isLocalhost ? "http://localhost:3000" : "https://aethera-eight.vercel.app",
+
+      redirectUri: isLocalhost
+        ? "http://localhost:3000" 
+        : "https://aethera-eight.vercel.app", // ✅ Make sure it's registered
+      knownAuthorities: ["aetheraparfum.ciamlogin.com"], // ✅ Required for B2C
     
     },
     cache: {
@@ -23,7 +31,8 @@ export const msalConfig = {
       "profile", 
       "email", 
       "offline_access", 
-      "297ac375-6408-43f6-bac5-e72e2c44b313/Aethera_access_api"
+      "api://297ac375-6408-43f6-bac5-e72e2c44b313/Aethera_access_api"
+      
       
     ]
   };
--- src/utils/auth/isAuthenticated.tsx
+++ src/utils/auth/isAuthenticated.tsx
@@ -1,7 +1,7 @@
-export function isAuthenticated(): boolean {
+/*export function isAuthenticated(): boolean {
     if (typeof window === 'undefined') return false; // Server-side guard
   
     const token = localStorage.getItem('accessToken'); // or sessionStorage or cookies
     return !!token;
   }
-  
+  */
