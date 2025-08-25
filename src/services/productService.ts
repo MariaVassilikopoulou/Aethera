07bdadb175f891902aafb61107a7973b5a9043ec
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-08-25 12:01:11 +0200

    product service

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,40 +1,34 @@
-// services/productService.ts
-
-
 export interface Product {
   id: string;
   name: string;
   description?: string;
   price: number;
   category?: string;
-  imageUrl:string
-
+  imageUrl: string;
 }
 
 export async function fetchProducts(token: string): Promise<Product[]> {
   try {
     const backendUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/Products`;
-    
+
     const response = await fetch(backendUrl, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-        
-        console.log("fetch token:", token);
-      if (!response.ok) {
-        throw new Error(` Failed to fetch: ${response.status}`);
-      }
-  
-      const result = await response.json();
-      return result; 
-    } catch (error) {
-      console.error(" Error fetching products:", error);
-      console.log("Calling /api/Products with token:", token);
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-      throw error;
+    console.log("Fetch token:", token);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.status}`);
     }
+
+    const result: Product[] = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    console.log("Calling /api/Products with token:", token);
+    throw error;
   }
-
-
+}
